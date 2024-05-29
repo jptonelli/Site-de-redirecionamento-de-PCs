f14d0341314cbb77bcb36603ce33743454fa4098
joao tonelli <joaopedrotonelli9@gmail.com>
2024-05-28 21:08:28 -0300

    Endereço CSS/JS

--- php/validaLogin.php
+++ php/validaLogin.php
@@ -1,19 +1,19 @@
 <?php
 
-if (empty($_POST['user'])) {
-    echo "E-mail é obrigatório e deve ser informado.";
-} elseif (empty($_POST['senha'])) {
-    echo "Senha é obrigatória e deve ser informada.";
-} else {
-    $email = $_POST['user'];
-    $senha = $_POST['senha'];
-    
-    if ($email !== 'teste') {
-        echo "E-mail inválido.";
-    } elseif ($senha !== 'teste') {
-        echo "Senha inválida.";
+    if (empty($_POST['user'])) {
+        echo "E-mail é obrigatório e deve ser informado.";
+    } elseif (empty($_POST['senha'])) {
+        echo "Senha é obrigatória e deve ser informada.";
     } else {
-        echo "Usuário logado com sucesso. Seja bem vindo.";
+        $email = $_POST['user'];
+        $senha = $_POST['senha'];
+        
+        if ($email !== 'teste') {
+            echo "E-mail inválido.";
+        } elseif ($senha !== 'teste') {
+            echo "Senha inválida.";
+        } else {
+            echo "Usuário logado com sucesso. Seja bem vindo.";
+        }
     }
-}
 ?>
